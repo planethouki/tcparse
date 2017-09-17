47de265a3af38e7e23da14fd7f233e24c4003f79
planethouki <syn_tok@hotmail.co.jp>
2017-09-17 14:16:01 +0900

    parse draft

---
+++ test.js
@@ -0,0 +1,37 @@
+const BIOME_INPUT_FOLDER = './WorldBiomesInput/';
+const BIOME_OUTPUT_FOLDER = './WorldBiomesOutput/';
+
+var fs = require('fs');
+var tckeys = JSON.parse(fs.readFileSync('./keys.json', 'utf8'));
+var biomeFiles = fs.readdirSync(BIOME_INPUT_FOLDER, 'utf8');
+
+var debug = new Array();
+var biomes = new Object();
+
+biomeFiles.forEach((biomeFileName, index, array) => {
+  console.log(biomeFileName);
+  var biome = new Object();
+  
+  var biomeFileTextReadAll = fs.readFileSync(BIOME_INPUT_FOLDER + biomeFileName,'utf8');
+  var biomeTextFilteredArray = biomeFileTextReadAll.replace(/\r\n?/g,'\n').split('\n')
+  .filter((element, index, array) => {
+    var flgSharp = !element.startsWith('#');
+    var flgEmpty = !element.length == 0;
+    return(flgSharp && flgEmpty);
+  })
+  biomeTextFilteredArray.forEach((biomeTextLine, index, array) => {
+    if (biomeTextLine.indexOf(': ') > -1) {
+      var b = biomeTextLine.indexOf(': ');
+      biome[biomeTextLine.substring(0, b)] = biomeTextLine.substring(b+1).trim();
+    } else {
+      var b = biomeTextLine.indexOf('(');
+      biome[biomeTextLine.substring(0, b)].push(biomeTextLine);
+    }
+  })
+  // console.log(biomeTextFilteredArray);
+  debug.push(biomeTextFilteredArray);
+  biomes[biomeFileName] = biome;
+});
+
+// fs.writeFileSync("./temp.txt", JSON.stringify(debug), 'utf8');
+fs.writeFileSync("./temp.txt", JSON.stringify(biomes), 'utf8');
